f044699d6f6d3de1add368bb5929fab46e783285
David <396866674@qq.com>
2017-11-08 13:52:40 +0800

    Add files via upload

--- install_llmp_frp.sh
+++ install_llmp_frp.sh
@@ -1,22 +1,47 @@
 ﻿#!/bin/sh
 
 echo -e "#======================================================================
-#  仅测试小米路由R1D
+#  安装前请确认您的路由器为小米路由器一代硬盘版（R1D）
 #  除R1D外其他路由暂不支持，强制安装后果自负
-#  全程只能输入y执行，如果输入n则直接退出，既安装失败
 #  Description: llmp+frp+kodexplorer一键安装
-#  Version: 1.0.3
+#  Version: 1.0.4
 #  Author: David
 #  web: http://91en.xyz/miwifi
 #======================================================================"
 
-echo "您确认要安装吗？请输入y/n"
+echo "您确认要安装吗？请输入y继续/n退出"
 read
 [ "$REPLY" = "y" -o "$REPLY" = "Y" ] || { echo "退出安装..."; exit; }
 
 echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>> 正在安装llmp请稍后 >>>>>>>>>>>>>>>>>>>>>>>>>>"
 wget http://91en.xyz/miwifi/llmp_install.bin -O /tmp/llmp_install.bin
 chmod +x /tmp/llmp_install.bin && /tmp/llmp_install.bin in
+
+echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>> 正在安装kodexplorer >>>>>>>>>>>>>>>>>>>>>>>>>>"
+wget http://91en.xyz/miwifi/kodexplorer.zip -O /tmp/kodexplorer.zip
+cd /tmp
+unzip /tmp/kodexplorer.zip -d /userdisk/data/wwwroot
+
+echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>> 添加服务随路由自启动 >>>>>>>>>>>>>>>>>>>>>>>>>>"
+chmod +x /etc/rc.local
+wget http://91en.xyz/miwifi/rc.local -O /etc/rc.local
+
+rm -rf /tmp/llmp_install.bin
+rm -rf /tmp/kodexplorer.zip
+
+echo -e "\033[32m #======================================================================
+#  到此已安装成功,安装结束后重启路由，本地可用“ip:8080”访问站点
+#  已添加服务随路由自动启动
+#  如安装失败请手动下载安装，工具和教程都在下面提供的链接里
+#  请复制下载地址：https://pan.baidu.com/s/1pLV07xT  密码:ndwu
+#  或访问Github:https://github.com/wo20ljj/miwifi
+#====================================================================== \033[0m \n"
+
+echo -e "\033[35m 如您的网络是大内网请输入y继续安装frp内网穿透，如果是公网ip则跳过直接输入n退出安装 \033[0m \n"
+sleep 2s
+echo -e "\033[35m 安装frp内网穿透客户端！请输入y继续/n退出 \033[0m \n"
+read
+[ "$REPLY" = "y" -o "$REPLY" = "Y" ] || { echo "退出安装..."; exit; }
 
 echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>> 正在安装frp请稍候 >>>>>>>>>>>>>>>>>>>>>>>>>>"
 wget http://91en.xyz/miwifi/frp_0.9.3_linux_arm.tar.gz -O /tmp/frp_0.9.3_linux_arm.tar.gz
@@ -32,16 +57,13 @@ echo -e "\033[33m #============================配置frpc.ini===================
 #  4、默认端口为local_port = 8080可不用更改
 #  5、按i键进入修改，完成后按Esc键，再输入:wq回车保存
 #  6、这里提供2个免费frp服务商，默认为frp1.chuantou.org
-#  server_addr = frp1.chuantou.org   or     frp.lu8.win
-#  privilege_token = www.xxorg.com   or     frp888
+#  ( server_addr = frp1.chuantou.org  |  privilege_token = www.xxorg.comor )
+#  ( server_addr = frp.lu8.win        |  privilege_token = frp888          )
 #========================配置过程可随时向上翻阅======================== \033[0m \n"
-echo -e "\033[35m 请认真阅读以上内容，继续配置吗？请输入y/n \033[0m \n"
+echo -e "\033[35m 请认真阅读以上内容，如配置错误将无法启动服务！请输入y继续 \033[0m \n"
 read
-[ "$REPLY" = "y" -o "$REPLY" = "Y" ] || { echo "退出安装..."; exit; }
+[ "$REPLY" = "y" -o "$REPLY" = "Y" ]
 
-echo -e "\033[33m 再次确认您已经阅读以上内容，继续配置吗？请输入y/n \033[0m \n"
-read
-[ "$REPLY" = "y" -o "$REPLY" = "Y" ] || { echo "退出安装..."; exit; }
 wget http://91en.xyz/miwifi/frpc.ini -O /userdisk/frpc/frpc.ini
 vi /userdisk/frpc/frpc.ini
 cd /userdisk/frpc/ && nohup ./frpc -c ./frpc.ini &
@@ -50,27 +72,15 @@ echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>> frpc.ini配置完成 >>>>>>>>>>>>>>>>>>>>>>>
 
 sleep 1s
 
-echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>> 添加服务随路由自启动 >>>>>>>>>>>>>>>>>>>>>>>>>>"
-chmod +x /etc/rc.local
-wget http://91en.xyz/miwifi/rc.local -O /etc/rc.local
-
-echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>> 正在安装kodexplorer >>>>>>>>>>>>>>>>>>>>>>>>>>"
-wget http://91en.xyz/miwifi/kodexplorer.zip -O /tmp/kodexplorer.zip
-cd /tmp
-unzip /tmp/kodexplorer.zip -d /userdisk/data/wwwroot
-
 echo -e "\033[32m #======================================================================
-#  安装完成（脚本没有自动检测是否安装成功，请手动检测）
-#  安装成功可用“域名”或“ip:8080”访问站点
+#  安装成功,如成功配置frp，可用“域名”或“ip:8080”访问站点
 #  无法访问请检查frpc.ini的配置
-#  安装过程无法下载可能是服务站点无法访问，可能需要翻墙
-#  安装失败请手动下载安装，工具和教程都在下面提供的链接里
+#  安装过程服务器压力较大，速度有限，请耐心等待
+#  如无法自动下载安装请手动下载，工具和教程都在下面提供的链接里
 #  请复制下载地址：https://pan.baidu.com/s/1pLV07xT  密码:ndwu
 #  或访问Github:https://github.com/wo20ljj/miwifi
 #====================================================================== \033[0m \n"
 
-rm -rf /tmp/install_llmp_frp.sh
 rm -rf /tmp/frp_0.9.3_linux_arm.tar.gz
-rm -rf /tmp/llmp_install.bin
-rm -rf /tmp/kodexplorer.zip
+rm -rf /tmp/install_llmp_frp.sh
 
