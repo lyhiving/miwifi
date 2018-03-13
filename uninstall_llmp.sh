40488a1b9c03ee90b28198cea18e5e5965ba64cd
David <396866674@qq.com>
2018-03-13 10:44:27 +0800

    Add files via upload

---
+++ uninstall_llmp.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+rm -rf /userdisk/llmp
+rm -rf /userdisk/frpc
+rm -rf /tmp/llmp_install.zip
+rm -rf /tmp/llmp_install.bin
+rm -rf /tmp/kodexplorer.zip
+rm -rf /tmp/frp_0.9.3_linux_arm.tar.gz
+rm -rf /tmp/install_llmp_frp.sh
+rm -rf /tmp/uninstall_llmp.sh
+reboot
+echo "完成！等待路由重启即可..."
